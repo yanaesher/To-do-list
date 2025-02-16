51c4c8fd946ab4cca631d8f08f1e7c4cf8bfda0f
Yana Seniuk <senyukyana26@gmail.com>
2025-02-16 19:22:23 +0100

    Updated script.js, style.css

--- script.js
+++ script.js
@@ -4,20 +4,44 @@ let addButton = document.getElementById('addButton');
 
 
 addButton.addEventListener('click', addTask);
+window.addEventListener('DOMContentLoaded', showTask);
 
 
 function addTask() {
-	if (inputBox.value === '') {
+	if (inputBox.value.trim() === '') {
 		alert('You must write task!');
+		return;
 	}
-	else {
-		let li = document.createElement('li');
-		li.classList.add('todo-app__list-container-item');
-		li.textContent = inputBox.value;
-		listContainer.append(li);
-	}
+	let li = document.createElement('li');
+	let span = document.createElement('span');
+	li.classList.add('todo-app__list-container-item');
+	li.textContent = inputBox.value;
+	listContainer.append(li);
+	span.innerHTML = '\u00d7';
+	li.append(span);
+	inputBox.value = '';
+	saveData();
 }
 
 
 
+listContainer.addEventListener('click', (e) => {
+	let target = e.target;
+	if (target.tagName === 'LI') {
+		target.classList.toggle('checked');
+		saveData();
+	}
+	else if (target.tagName === 'SPAN') {
+		target.parentElement.remove();
+		saveData();
+	}
+});
+
+function saveData() {
+	localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask() {
+	listContainer.innerHTML = localStorage.getItem('data');
+}
 
